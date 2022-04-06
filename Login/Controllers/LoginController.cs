512ae69f05ad88888b626cb9aeacf10bc431e7be
Fernando Notebook <fcgraciano@gmail.com>
2022-04-06 11:07:30 -0300

    Aula de login

--- Login/Controllers/LoginController.cs
+++ Login/Controllers/LoginController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Login.Controllers
@@ -11,6 +13,44 @@ namespace Login.Controllers
         public IActionResult Entrar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Entrar(string usuario,string senha)
+        {
+            if(usuario == "admin" && senha == "123")
+            {
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, usuario));
+                claims.Add(new Claim(ClaimTypes.Sid, "10"));
+                claims.Add(new Claim(ClaimTypes.Role, "AcessarTela"));
+
+                var userIdentity = new ClaimsIdentity(claims, "Autenticacao");
+
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+
+                await HttpContext.SignInAsync("Autenticacao", principal, new AuthenticationProperties
+                {
+                    ExpiresUtc = new DateTimeOffset(DateTime.Now.AddHours(7)),
+                    IsPersistent = true
+
+                }); 
+
+                return Redirect("/");
+            }
+            else
+            {
+                TempData["erro"] = "Usuario e Senha inválido";
+                return View();
+            }
+            
+        }
+
+        public async Task<IActionResult> Logoff()
+        {
+            await HttpContext.SignOutAsync("Autenticacao");
+            ViewData["ReturnUrl"] = "/";
+            return Redirect("/Login/Entrar");
         }
     }
 }
--- Login/Startup.cs
+++ Login/Startup.cs
@@ -23,6 +23,13 @@ namespace Login
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddAuthentication("Autenticacao")
+                .AddCookie("Autenticacao", option =>
+                {
+                    option.LoginPath = "/Login/Entrar";
+                    option.AccessDeniedPath = "/Login/ops";
+                }
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
